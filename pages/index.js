8e633d6b1931de8898a2ac398eaadb2ffd4fc4fd
Nima Pourjafar <nima.pourjafar123@gmail.com>
2021-06-05 03:47:31 -0400

    addded index mostly

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
       <div className="absolute w-full h-full bg-background">
         <div className="flex items-center justify-left ml-10 h-64">
-          <h1 className="text-9xl font-extrabold text-white">
+          <h1 className="text-9xl font-extrabold text-white pt-44">
             Hey, I'm{" "}
             <span
               className="bg-clip-text text-transparent bg-gradient-to-r from-navyblue to-skyblue animate-gradient-x"
