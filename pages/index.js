b639359873f34fc6af2a0a6dc8b88c0128bdb667
Nima Pourjafar <nima.pourjafar123@gmail.com>
2021-06-10 00:21:30 -0400

    added waves

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import wavestyles from "../styles/waves.module.css"
 import Typical from "react-typical";
 
 export default function Home() {
@@ -11,9 +12,9 @@ export default function Home() {
       </Head>
       <div className="absolute w-full h-full bg-background overflow-hidden">
         <div className="flex flex-col justify-left ml-12 md:ml-28 h-64 mt-24">
-          <h1 className="text-8xl md:text-6xl font-extrabold text-white">
+          <h1 className="text-8xl md:text-10xl font-extrabold text-white">
             Hey, I'm <br/> 
-            <span className="bg-clip-text text-transparent bg-gradient-to-r from-navyblue to-skyblue animate-gradient-xy font-black md:text-fat">NIMA!</span>
+            <span className="bg-clip-text text-transparent bg-gradient-to-r from-navyblue via-blue-500 to-skyblue animate-gradient-xy font-black text-9xl md:text-fat">NIMA!</span>
           </h1>
           <h2 className="text-4xl text-white font-semibold">
             I ENJOY{" "}
@@ -34,7 +35,11 @@ export default function Home() {
             />
           </h2>
         </div>
-      </div>
+        <div className={wavestyles.ocean}>
+          <div class={wavestyles.wave}></div>
+          <div class={wavestyles.wave}></div>
+        </div>
+      </div>      
     </div>
   );
 }
