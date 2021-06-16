aeb597b9c2847aa7ef1d5a28eaf51a2ab922e6d8
Nima Pourjafar <nima.pourjafar123@gmail.com>
2021-06-15 22:20:04 -0400

    image ?!??!?!

--- components/Navbar.js
+++ components/Navbar.js
@@ -20,7 +20,7 @@ const Navbar = () => {
               </div>
               <div className="hidden md:block">
                 <div className="ml-14 flex items-baseline space-x-8">
-                  <Link href="/">
+                  <Link href="/about">
                     <a className="font-bold  text-white px-3 py-2 rounded-md text-lg duration-500 hover:bg-gray-700">
                       About
                     </a>
--- components/SpotifyPlaying.js
+++ components/SpotifyPlaying.js
@@ -6,32 +6,35 @@ export default function SpotifyPlaying() {
   const { data } = useSWR("/api/spotify", fetcher);
 
   return (
-    <div className="mx-10 hidden lg:block px-2 pt-1 pb-px rounded-md duration-500 hover:bg-gray-700">
-      <div className="inline-flex flex-col items-center sm:flex-row w-full max-w-full truncate">
-        {data?.songUrl ? (
-          <div className="flex flex-auto items-center">
-            <img className="mx-2" style={{height:"auto",width:"auto",maxWidth:"40px"}} src={data.albumImageUrl} />
-            <a
-              className="text-skyblue font-extrabold max-w-max truncate"
-              href={data.songUrl}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {data.title}
-            </a>            
-          </div>
-        ) : (
-          <p className="text-gray-800 font-medium">
-            
+    <>
+      
+      <div className="mx-10 hidden lg:block px-2 pt-1 pb-px rounded-md duration-500 hover:bg-gray-700">
+        <div className="inline-flex flex-col items-center sm:flex-row w-full max-w-full truncate">
+          {data?.songUrl ? (
+            <div className="flex flex-auto items-center">
+              <img
+                className="mx-2"
+                style={{ height: "auto", width: "auto", maxWidth: "40px" }}
+                src={data.albumImageUrl}
+              />
+              <a
+                className="text-skyblue font-extrabold max-w-max truncate"
+                href={data.songUrl}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {data.title}
+              </a>
+            </div>
+          ) : (
+            <p className="text-gray-300 font-medium">Spotify</p>
+          )}
+          <span className="mx-2 text-gray-500  hidden sm:block">{" – "}</span>
+          <p className="text-gray-400  max-w-max truncate">
+            {data?.artist ?? "Offline"}
           </p>
-        )}
-        <span className="mx-2 text-gray-500  hidden sm:block">
-          {" – "}
-        </span>
-        <p className="text-gray-400  max-w-max truncate">
-          {data?.artist ?? " "}
-        </p>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
