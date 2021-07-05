88a89b18993f5930ac5141628cc59fe67de8dd54
Nima Pourjafar <nima.pourjafar123@gmail.com>
2021-07-05 12:01:04 -0400

    bro

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -25,11 +25,9 @@ const portfolio = () => {
         <h1 className="py-24 text-center mx-auto font-black text-6xl md:text-8xl bg-clip-text text-transparent bg-gradient-to-r from-navyblue via-blue-500 to-skyblue animate-gradient-xy">
           Portfolio
         </h1>
-        <div className="bg-blue-700 duration-500 hover:bg-blue-800 hover:shadow-xl text-white font-bold py-4 px-4 rounded mx-auto w-56 text-center mb-4">
-          <a href="https://drive.google.com/file/d/1d3gJfaOPk6YVslpFA_pu8VB2Uc8S0kVK/view?usp=sharing">
-            Click me for my resume!
-          </a>
-        </div>
+        <a href="https://drive.google.com/file/d/1d3gJfaOPk6YVslpFA_pu8VB2Uc8S0kVK/view?usp=sharing">
+          <h2 className="w-60 mb-10 py-2 rounded-2xl text-center mx-auto font-black text-xl md:text-3xl bg-gradient-to-r from-navyblue via-blue-500 to-skyblue animate-gradient-xy duration-500 hover:text-white hover:shadow-xl">Check out my resume here</h2>
+        </a>
 
         <VerticalTimeline animate={false}>
           {timelineElements.map((element) => {
